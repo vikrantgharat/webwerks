f9904ecaa38a03a771793d93dd9a7ff2a8cfae24
Meera Naringrekar <meera.naringrekar@wwindia.com>
2017-10-16 14:34:27 +0530

    Data center sub pages & Careers page

--- src/js/custom.js
+++ src/js/custom.js
@@ -24,15 +24,17 @@ $(document).ready(function() {
         });
     }
 
+
+
     $window.on('scroll resize', check_if_in_view);
     $window.trigger('scroll');
     //on scroll slide effect
 
-    
+  });  
 
-    $('.hidden').hide()
+    // $('.hidden').hide();
 
-    
+$(document).ready(function() {    
     //form click effect
     $('.form-click').click(function(event) {
         event.stopPropagation();
@@ -159,7 +161,58 @@ $(document).ready(function() {
         },
     });
 
-
-   
-
 });
+
+//Gallery Slider for Datacenters
+ $(document).ready(function () {
+  // reference for main items
+  var slider = $('#slider');
+  // reference for thumbnail items
+  var thumbnailSlider = $('#thumbnailSlider');
+  //transition time in ms
+  var duration = 500;
+
+  // carousel function for main slider
+  slider.owlCarousel({
+   loop:false,
+   nav:false,
+   items:1
+  }).on('changed.owl.carousel', function (e) {
+   //On change of main item to trigger thumbnail item
+   thumbnailSlider.trigger('to.owl.carousel', [e.item.index, duration, true]);
+  });
+
+  // carousel function for thumbnail slider
+  thumbnailSlider.owlCarousel({
+   loop:false,
+   center:true, //to display the thumbnail item in center
+   nav:false,
+   responsive:{
+    0:{
+     items:3
+    },
+    600:{
+     items:4
+    },
+    1000:{
+     items:6
+    }
+   }
+  }).on('click', '.owl-item', function () {
+   // On click of thumbnail items to trigger same main item
+   slider.trigger('to.owl.carousel', [$(this).index(), duration, true]);
+
+  }).on('changed.owl.carousel', function (e) {
+   // On change of thumbnail item to trigger main item
+   slider.trigger('to.owl.carousel', [e.item.index, duration, true]);
+  });
+
+
+  //These two are navigation for main items
+  $('.slider-right').click(function() {
+   slider.trigger('next.owl.carousel');
+  });
+  $('.slider-left').click(function() {
+   slider.trigger('prev.owl.carousel');
+  });
+ });
